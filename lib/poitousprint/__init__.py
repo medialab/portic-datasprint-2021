19affd66b1e8f35724d9734e55f9c2c11994c8ee
Robin de Mourat <robin.demourat@gmail.com>
2021-03-21 11:37:46 +0100

    add cooccurence graph util

--- lib/poitousprint/__init__.py
+++ lib/poitousprint/__init__.py
@@ -1,3 +1,3 @@
 from poitousprint.toflit import Toflit
 from poitousprint.portic import Portic
-from poitousprint.utils import nest_toflit18_flow, nest_portic_pointcall
+from poitousprint.utils import nest_toflit18_flow, nest_portic_pointcall, build_cooccurence_graph
--- lib/poitousprint/utils.py
+++ lib/poitousprint/utils.py
@@ -1,3 +1,6 @@
+
+import networkx as nx
+
 def print_classification(input, classification, displace):
     if 'name' in classification:
         input += displace + '- ' + classification['name'] + '(' + classification['slug'] + ')' + '\n'
@@ -294,3 +297,85 @@ def nest_portic_pointcall(pointcall):
 
   return output
 
+
+
+def build_cooccurence_graph(data, key_1, key_2, **kwargs):
+    # créer un graphe
+    Graph = nx.Graph()
+
+    # créer des dict pour les deux types de noeuds et les liens
+    key1_uniq = {}
+    key2_uniq = {}
+    edges_uniq = {}
+    default_params = {
+        "color_1": "rgb(0, 255, 0)",
+        "color_2": "rgb(255, 0, 0)",
+        "node_min_size": 1,
+        "node_max_size": 10
+    }
+    params = {
+        *default_params,
+        *kwargs
+    }
+    
+    # remplir les dicts
+    for datum in data:
+        if key_1 in datum and key_2 in datum:
+            value_1 = datum[key_1] if datum[key_1] is not None else "undefined"
+            value_2 = datum[key_2] if datum[key_2] is not None else "undefined"
+            value_1_id = key_1 + "_" + value_1
+            value_2_id = key_2 + "_" + value_2
+            if value_1_id in key1_uniq:
+                key1_uniq[value_1_id] = {**key1_uniq[value_1_id], "size": key1_uniq[value_1_id]["size"] + 1}
+            else:
+               key1_uniq[value_1_id] = {
+                   "type": key_1, 
+                   "name": value_1, 
+                   "color": params["color_1"],
+                   "size": 1
+               }
+            if value_2_id in key2_uniq:
+                key2_uniq[value_2_id] = {**key2_uniq[value_2_id], "size": key2_uniq[value_2_id]["size"] + 1}
+            else:
+               key2_uniq[value_2_id] = {
+                   "type": key_2, 
+                   "name": value_2, 
+                   "color": params["color_2"],
+                   "size": 1
+               }
+            edge_footprint = value_1_id + "-" + value_2_id
+            if edge_footprint in edges_uniq:
+                edges_uniq[edge_footprint]["weight"] += 1
+            else:
+                edges_uniq[edge_footprint] = {
+                    "source": value_1_id,
+                    "target": value_2_id,
+                    "weight": 1
+                }
+    # concaténer les deux dicts de noeuds en un seul
+    all_nodes = key1_uniq
+    all_nodes.update(key2_uniq)
+    # applatir et formatter les noeuds
+    nodes = []
+    for key, node in all_nodes.items():
+        nodes.append((key, node))
+    edges = []
+
+    for key, edge in edges_uniq.items():
+        edges.append((edge["source"], edge["target"], {"weight": edge["weight"]}))
+        
+    # ajuster la taille des noeuds en fonction d'un min et d'un max donnés
+    domain_min_nodes_size = min([node[1]['size'] for node in nodes])
+    domain_max_nodes_size = max([node[1]['size'] for node in nodes])
+    range_in_nodes_size = [params["node_min_size"], params["node_max_size"]]
+    nodes_size_mapping_params = [domain_min_nodes_size, domain_max_nodes_size, *range_in_nodes_size]
+
+    for node in nodes:
+        node[1]["size"] = map_value(node[1]["size"], *nodes_size_mapping_params)
+        node[1]["label"] = node[1]["name"]
+
+
+    Graph.add_nodes_from(nodes)
+    Graph.add_edges_from(edges)
+
+    return Graph

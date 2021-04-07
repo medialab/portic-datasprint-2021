6904b30fdbedf73168e32818fc1aa534a5f9573c
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2021-04-07 17:10:54 +0200

    cleanup

--- productions/equipe_2/produits/get_products.py
+++ productions/equipe_2/produits/get_products.py
@@ -4,16 +4,13 @@ import os
 import sys
 import csv
 import json
-from pprint import pprint
 from collections import Counter, defaultdict
 import networkx as nx
 from slugify import slugify
-from poitousprint import Portic, Toflit, get_pointcalls_commodity_purposes_as_toflit_product
+from poitousprint import Portic, get_pointcalls_commodity_purposes_as_toflit_product
 
-portic = Portic()
-#toflit = Toflit()
 
-def get_navigo_products(admiralties, year, filter_only_out=True, products=None):
+def get_navigo_products(admiralties, year, filter_only_out=True):
     cachedata = os.path.join(CACHEDIR, "portic_pointcalls_%s_%s.json" % (year, "_".join([slugify(a) for a in sorted(admiralties)])))
     try:
         with open(cachedata) as f:
@@ -21,24 +18,23 @@ def get_navigo_products(admiralties, year, filter_only_out=True, products=None):
             print('USING cached data from PORTIC for admiralties "%s" in %s' % (", ".join(admiralties), year))
     except:
         print('DOWNLOADING PORTIC data for admiralties "%s" in %s' % (", ".join(admiralties), year))
+        portic = Portic()
         pointcalls = portic.get_pointcalls(year=year, pointcall_admiralty=admiralties)
         with open(cachedata, "w") as f:
             json.dump(pointcalls, f)
-    if not products:
-        products = {
-            "portic_default": defaultdict(Counter),
-            "portic_standardized_fr": defaultdict(Counter),
-            "toflit_simplification": defaultdict(Counter),
-            "toflit_revolution": defaultdict(Counter),
-            "toflit_aggregate": defaultdict(Counter)
-        }
+    products = {
+        "portic_default": defaultdict(Counter),
+        "portic_standardized_fr": defaultdict(Counter),
+        "toflit_simplification": defaultdict(Counter),
+        "toflit_revolution": defaultdict(Counter),
+        "toflit_aggregate": defaultdict(Counter)
+    }
     toflit_classifications = [
         ("toflit_simplification", "product_simplification"),
         ("toflit_revolution", "product_revolutionempire"),
         ("toflit_aggregate", "product_RE_aggregate"),
     ]
-    for idx, classif in enumerate(toflit_classifications):
-        key, toflit_classif = classif
+    for idx, (key, toflit_classif) in enumerate(toflit_classifications):
         print('WORKING on PORTIC data for admiralties "%s" in %s with TOFLIT classification "%s"' % (", ".join(admiralties), year, toflit_classif))
         pointcalls_as_toflit = get_pointcalls_commodity_purposes_as_toflit_product(pointcalls, product_classification=toflit_classif)
         for pc in pointcalls_as_toflit:
@@ -46,8 +42,6 @@ def get_navigo_products(admiralties, year, filter_only_out=True, products=None):
                 continue
             port = pc["toponyme_fr"]
             for c in pc["commodity_purposes"]:
-                if not c["commodity_purpose"]:
-                    continue
                 if not idx:
                     products["portic_default"][port][c["commodity_purpose"]] += 1
                     products["portic_standardized_fr"][port][c["commodity_standardized_fr"]] += 1
@@ -95,9 +89,11 @@ def build_bipartite_networks(products, year, filter_only_out=True):
 
 if __name__ == "__main__":
     admiralties = ["La Rochelle", "Marennes", "Sables-d’Olonne"]
+
     year = 1789
     if len(sys.argv) > 1:
         year = sys.argv[1]
+
     filter_only_out = True
     if len(sys.argv) > 2:
         filter_only_out = False
